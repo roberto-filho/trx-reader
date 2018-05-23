2d452228d591552ad383d163de7cb81192265e9e
Roberto Filho <roberto.w.filho@gmail.com>
2018-05-22 22:28:41 -0300

    Enabling CORS

--- startServer.js
+++ startServer.js
@@ -2,12 +2,23 @@ const BankResource = require('./resources/BankResource');
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// Read env variables
+const port = process.env['PORT'] || 3000;
+const corsAddress = process.env['CORS'] || 'http://localhost:3000';
+
+// Setup cors
+var corsOptions = {
+  origin: corsAddress,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions));
 
 const resource = new BankResource();
 
 // Call the register paths
 resource.registerPaths(app);
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
